b3ab2d40178abcb5193d9370a9e22dc2977def06
Vsevolod Polyakov <vsevolod.polyakov@grammarly.com>
2015-09-04 16:11:18 +0300

    add version with compile

--- Library/Formula/rocker-compose.rb
+++ Library/Formula/rocker-compose.rb
@@ -1,15 +1,20 @@
 class RockerCompose < Formula
   desc "Docker tool for deploying apps composed of multiple containers."
   homepage "https://github.com/grammarly/rocker-compose"
-  url "https://github.com/grammarly/rocker-compose/releases/download/0.1.0/rocker-compose-0.1.0_darwin_amd64.tar.gz"
+  url "https://github.com/grammarly/rocker-compose.git", :tag => "0.1.0"
+  head "https://github.com/grammarly/rocker-compose.git"
   version "0.1.0"
-  sha256 "9106fd69de0a2deaa71af220b0355e6d562ce3069a57c29a0b661eba9fb5e992"
+
+  depends_on "go" => :build
 
   def install
-    bin.install "rocker-compose"
+    ENV["GOPATH"] = "#{Dir.pwd}/vendor:#{Dir.pwd}"
+    system "make", "local-binary"
+    bin.install "bin/rocker-compose"
   end
 
   test do
-    system "#{bin}/rocker-compose", "--version"
+    shell "#{bin}/rocker-compose --version"
   end
 end
+
